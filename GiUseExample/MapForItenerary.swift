02436730c0c85103cc46d8fdbe9a9fa5d0818ae9
David Lindell <spam@spam.spam>
2017-11-09 14:02:25 -0500

    Add a new map feature.

---
+++ GiUseExample/MapForItenerary.swift
@@ -0,0 +1,13 @@
+//
+//  MapForItenerary.swift
+//  GiUseExample
+//
+//  Created by David Lindell on 2017-11-09.
+//  Copyright © 2017 LOViews Development. All rights reserved.
+//
+
+import UIKit
+
+class MapForItenerary: NSObject {
+    
+}
